80d26522e22a8092569e693eeec5884e832e999a
Pablo Jimenez <jup_jim_7@hotmail.com>
2021-04-29 22:07:55 -0500

    DGV Pantalla de Pedidos V1.2

--- HerreriaZar/Pedidos.vb
+++ HerreriaZar/Pedidos.vb
@@ -1,11 +1,12 @@
 ﻿
 
+Imports MySql.Data.MySqlClient
+
 Public Class Pedidos
 
 
     Private Sub Pedidos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim pedidos As New ClasePedidos
-        pedidos.poblarDGVPedidos(DGVPedidos)
+
 
     End Sub
 
@@ -46,6 +47,43 @@ Public Class Pedidos
 
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGVPedidos.CellContentClick
+
+    End Sub
+
+    Private Sub BotonActualizar_Click(sender As Object, e As EventArgs) Handles BotonActualizar.Click
+        'Botón de guardar, cuando se quiere registrar la información, validamos que se haya
+        'capturado texto en las cajas de texto
+
+        'Declaracion de variables necesarias'
+        Dim cnx As MySqlConnection = New MySqlConnection
+        Dim comando As MySqlCommand = New MySqlCommand
+
+        comando.Connection = cnx
+
+        'Probar conexion'
+        Try
+            cnx.ConnectionString = "Server = localhost; Database = herreriazar; Uid = root; Pwd =zP8HlxqCBwCFHcHz"
+            cnx.Open()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        comando.CommandText = "SELECT cc.nombre as cliente, vg.id as ID_Venta, vg.fecha as fecha_pedido, vg.fecha_b as fecha_entrega, vg.total, vg.anticipo" & " 
+                                                    FROM venta_general as vg JOIN catalogo_clientes as cc ON (cc.id = vg.clientes_fk);"
+
+        Dim dt As DataTable = New DataTable
+        Dim da As MySqlDataAdapter = New MySqlDataAdapter(comando)
+
+        da.Fill(dt)
+        DGVPedidos.DataSource = dt
+        cnx.Close()
+
+
+
+
+
+        'Invocamos al metodo para poblar con los datos al DGV
 
     End Sub
 End Class
